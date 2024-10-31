ad294c4ebb39d401a13c6049340a5007ed193be1
Dmitriy Punenko <punenkodmitriy@gmail.com>
2024-10-31 14:21:07 +0500

    API + changed logo for footer

--- script.js
+++ script.js
@@ -1,63 +1,70 @@
-let burgerButton = document.getElementById('buttonBurger');
-let burger = document.getElementById('burger');
-let buttonBurgerClose = document.getElementById('buttonBurgerClose');
-let buttonForOtherLinks = document.getElementById('buttonForOtherLinks');
-let otherPc = document.getElementById('otherPc');
-let burgerButtonForOther = document.getElementById('burgerButtonForOther');
-let burgerAdditionalMenu = document.getElementById('burgerAdditionalMenu');
-let form = document.getElementById('form');
+const burgerButton = document.getElementById('buttonBurger');
+const burger = document.getElementById('burger');
+const buttonBurgerClose = document.getElementById('buttonBurgerClose');
+const buttonForOtherLinks = document.getElementById('buttonForOtherLinks');
+const otherPc = document.getElementById('otherPc');
+const burgerButtonForOther = document.getElementById('burgerButtonForOther');
+const burgerAdditionalMenu = document.getElementById('burgerAdditionalMenu');
+const form = document.getElementById('form');
 
+if (burgerButton) {
+    burgerButton.addEventListener('click', function(e){
+        console.log('Button clicked');
+        burger.classList.toggle('active');
+    });
+}
 
-
-burgerButton.addEventListener('click', function(e){
-    console.log('Button clicked');
-    burger.classList.toggle('active');
-});
-
-buttonBurgerClose.addEventListener('click', function(e){
-    console.log('Button close');
-    console.log(buttonBurgerClose)
-    burger.classList.remove('active');
-});
+if (buttonBurgerClose) {
+    buttonBurgerClose.addEventListener('click', function(e){
+        console.log('Button close');
+        console.log(buttonBurgerClose)
+        burger.classList.remove('active');
+    });    
+}
 
 // scripts for making other button(menu which will be opebable) and burger
-buttonForOtherLinks.addEventListener('click', function(e){
-    e.preventDefault();
-    console.log('Button for other links clicked');
-    otherPc.classList.toggle('active');
-});
-
-burgerButtonForOther.addEventListener('click', function(e){
-    console.log(e);
-    burgerAdditionalMenu.classList.toggle('active');
-});
-
-form.addEventListener('submit', function(e) {
-    console.log(form);
-        const firstName = document.getElementById('first-name').value;
-        const lastName = document.getElementById('last-name').value;
-        const age = document.getElementById('age').value;
-        const email = document.getElementById('email').value;
-
-
-        let gender;
-        if (document.getElementById('male').checked) {
-            gender = 'male';
-        } else if (document.getElementById('female').checked) {
-            gender = 'female';
-        } else if (document.getElementById('other').checked) {
-            gender = 'other';
-        }
-
-        console.log('Name:', firstName);
-        console.log('Surname:', lastName);
-        console.log('Age:', age);
-        console.log('Email:', email);
-        console.log('Gender:', gender);
+if (buttonForOtherLinks) {
+    buttonForOtherLinks.addEventListener('click', function(e){
         e.preventDefault();
-});
-console.log(burgerButtonForOther);
-console.log(form);
+        console.log('Button for other links clicked');
+        otherPc.classList.toggle('active');
+    });
+    
+}
+if(buttonForOtherLinks) {
+    burgerButtonForOther.addEventListener('click', function(e){
+        console.log(e);
+        burgerAdditionalMenu.classList.toggle('active');
+    });    
+}
+if (form) {
+    form.addEventListener('submit', function(e) {
+        console.log(form);
+            const firstName = document.getElementById('first-name').value;
+            const lastName = document.getElementById('last-name').value;
+            const age = document.getElementById('age').value;
+            const email = document.getElementById('email').value;
+    
+    
+            let gender;
+            if (document.getElementById('male').checked) {
+                gender = 'male';
+            } else if (document.getElementById('female').checked) {
+                gender = 'female';
+            } else if (document.getElementById('other').checked) {
+                gender = 'other';
+            }
+    
+            console.log('Name:', firstName);
+            console.log('Surname:', lastName);
+            console.log('Age:', age);
+            console.log('Email:', email);
+            console.log('Gender:', gender);
+            e.preventDefault();
+    });
+    console.log(burgerButtonForOther);
+    console.log(form);
+}
 
 function calculate() {
     const value1 = parseFloat(document.getElementById("value1").value);
@@ -103,7 +110,9 @@ function calculate2() {
         } else if (operation2 == "cot") {
             if(angle == 0) {
                 result2 = 'Cotangent is undefined';
-            }   else {
+            } else if(angle = 180) {
+                result2 = "cotangent is undefined";
+            }  else {
                 let radians = angle * (Math.PI / 180);
                 result2 = (1 / Math.tan(radians)).toFixed(4);
             }
@@ -112,4 +121,29 @@ function calculate2() {
     document.getElementById("result2").innerText = `Result: ${result2}`;
     console.log(angle, operation2)
 }
+
+// API
+const url = "https://api.open-meteo.com/v1/forecast?latitude=52.52,35.6895,51.5085&longitude=13.41,139.6917,-0.1257&hourly=temperature_2m&daily=temperature_2m_max,temperature_2m_min"
+const Petro = document.getElementById("Petro");
+const Tokyo = document.getElementById("Tokyo");
+const London = document.getElementById("London");
+async function getWeather() {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) { 
+      throw new Error(`Ошибка: ${response.status}`);
+    }
+    const data = await response.json();
+    let avgTemperature1 = Math.ceil(data[0].daily.temperature_2m_max.reduce((acc, el) => acc + el, 0) / data[0].daily.temperature_2m_max.length);
+    let avgTemperature2 = Math.ceil(data[1].daily.temperature_2m_max.reduce((acc, el) => acc + el, 0) / data[1].daily.temperature_2m_max.length);
+    let avgTemperature3 = Math.ceil(data[2].daily.temperature_2m_max.reduce((acc, el) => acc + el, 0) / data[2].daily.temperature_2m_max.length);
+    Petro.innerText = `Avg temp: ${avgTemperature1}°C`;
+    Tokyo.innerText = `Avg temp: ${avgTemperature2}°C`;
+    London.innerText = `Avg temp: ${avgTemperature3}°C`;
+} catch (error) {
+    console.error('Ошибка запроса:', error);
+  }
+}
+
+getWeather();
 
